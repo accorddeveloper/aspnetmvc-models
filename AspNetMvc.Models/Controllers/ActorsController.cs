ac3817068d67de5233261da0ed13bcb7a50b9919
William Willing <william@williamwilling.com>
2015-03-12 15:05:27 +0100

    Created an action and a view to show a list of actors.

--- AspNetMvc.Models/Controllers/ActorsController.cs
+++ AspNetMvc.Models/Controllers/ActorsController.cs
@@ -6,9 +6,9 @@ namespace AspNetMvc.Models
     {
         public ActionResult Details()
         {
-			// Here we create information about a single actor. In most cases, you would get this
-			// data from an external source, like a database, but for the purposes of this example,
-			// we just create the object directly in code.
+            // Here we create information about a single actor. In most cases, you would get this
+            // data from an external source, like a database, but for the purposes of this example,
+            // we just create the object directly in code.
             var actor = new Actor()
             {
                 FirstName = "Hugh",
@@ -16,9 +16,71 @@ namespace AspNetMvc.Models
                 Nationality = "Australian"
             };
 
-			// You can pass a model from the action to the view by passing it as a parameter to
+            // You can pass a model from the action to the view by passing it as a parameter to
             // View().
-			return View(actor);
+            return View(actor);
+        }
+
+        public ActionResult Index()
+        {
+            // Here we create information about a number of actors. In most cases, you would get
+            // this data from an external source, like a database, but for the purposes of this
+            // example, we just create the array directly in code.
+            var actors = new Actor[]
+            {
+                new Actor()
+                {
+                    FirstName = "Hugh",
+                    LastName = "Jackman",
+                    Nationality = "Australian"
+                },
+
+                new Actor()
+                {
+                    FirstName = "Gary",
+                    LastName = "Oldman",
+                    Nationality = "English"
+                },
+
+                new Actor()
+                {
+                    FirstName = "Keira",
+                    LastName = "Knightley",
+                    Nationality = "English"
+                },
+
+                new Actor()
+                {
+                    FirstName = "Morgan",
+                    LastName = "Freeman",
+                    Nationality = "American"
+                },
+
+                new Actor()
+                {
+                    FirstName = "Jennifer",
+                    LastName = "Connelly",
+                    Nationality = "American"
+                },
+
+                new Actor()
+                {
+                    FirstName = "Russell",
+                    LastName = "Crowe",
+                    Nationality = "New Zealand"
+                },
+
+                new Actor()
+                {
+                    FirstName = "William",
+                    LastName = "Shatner",
+                    Nationality = "Canadian"
+                }
+            };
+
+            // You can pass a collection of models from the action to the view by passing the
+            // collection as a parameter to View().
+            return View(actors);
         }
     }
 }
