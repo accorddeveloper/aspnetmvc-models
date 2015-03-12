2735fcfe6c088464e47621ce43055800d28fb0f8
William Willing <william@williamwilling.com>
2015-03-12 13:50:17 +0100

    Created a model that describes an actor.

---
+++ AspNetMvc.Models/Models/Actor.cs
@@ -0,0 +1,11 @@
+﻿namespace AspNetMvc.Models
+{
+    // A model describes the characteristics of an entity the application needs to work with. In
+    // this case, the Actor-model describes everything we want to know about an actor.
+    public class Actor
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Nationality { get; set; }
+    }
+}
